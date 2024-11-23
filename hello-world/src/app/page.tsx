8eece13ade0c9502a15a3a5e618eddc395bdfd27
ayeshafahad2 <ayeshafahad661@gmail.com>
2024-11-23 11:06:15 +0500

    Initial commit after reinitialization

---
+++ hello-world/src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>HELLO WORLD OF NEXTJS</h1>
+      <h1>FROM AYESHA FAHAD KHAKWAANI</h1>
+      <p> Sir Asharib</p>
+      <p> Monday 2pm -5pm</p>
+    </div>
+  )
+}
+
+export default Home
